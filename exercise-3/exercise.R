3db2af60aef09a3bc0a5854b6e18ecf6f8c4b7e3
franczloo <franczloo@gmail.com>
2016-10-11 13:50:13 -0700

    ex3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,17 +1,28 @@
 # Exercise 3: Vector and function practice
 
-# Create a vector `marbles` with 6 different colors in it (representing marbles)
+#install.packages("stringr")
+#library(stringr)
 
+# Create a vector `marbles` with 6 different colors in it (representing marbles)
+marbles <- c('blue','orange','red','purple','white','black')
 
 # Use the `sample` function to select a single marble
-
+sample(marbles,1)
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
+MarbleGame <- function(guess) {
+  marb <- sample(marbles,1)
+  if (guess == marb) {
+    return ("You win")
+  } else {
+    return ("Nope")
+  }
+}
 
-
+MarbleGame('blue')
 # Play the marble game!
 
 
